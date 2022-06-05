c885bce253fa4690b869324cfc48f1a49e50432d
Nikita Hudz <nikita.gudz8@gmail.com>
2022-06-05 17:16:27 +0300

    start hw 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const selectGalleryList = document.querySelector('.gallery');
+// console.log(selectGalleryList);
+
+const createGaletyElements = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
+  })
+  .join('');
+
+console.log(createGaletyElements);
+
+selectGalleryList.innerHTML = createGaletyElements;
+// selectGalleryList.insertAdjacentHTML('beforeend', createGaletyElements);
